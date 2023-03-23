12af02617b06ac3f7440c3e22c3da98b60229f3e
rich-back <backhouse.richard@gmail.com>
2023-03-23 13:07:53 +0000

    Sanity client config updated

--- sanity.ts
+++ sanity.ts
@@ -6,9 +6,9 @@ export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION // "2022-11-16"
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion, // https://www.sanity.io/docs/api-versioning
+  projectId: 'g467rhdy',
+  dataset: 'production',
+  apiVersion: '2021-10-21', // https://www.sanity.io/docs/api-versioning
   useCdn: true, // if you're using ISR or only static generation at build time then you can set this to `false` to guarantee no stale content
 })
 
